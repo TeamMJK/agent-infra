cac8e5894acd7c184534dfd27ee6bf70df5b06c8
sqautboy <migonyoung01@gmail.com>
2025-08-01 15:02:48 +0900

    feat: modify NAT Gateway - single AZ for cost optimization

--- network/main.tf
+++ network/main.tf
@@ -69,17 +69,14 @@ resource "aws_subnet" "private_app" {
 }
 
 resource "aws_route_table" "private_app" {
-  # for_each = aws_nat_gateway.main  # NAT Gateway를 사용하지 않는 경우 주석 처리
-
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    # nat_gateway_id = each.value.id # NAT Gateway를 사용하지 않는 경우 주석 처리
+    nat_gateway_id = aws_nat_gateway.main.id
   }
 
   tags = {
-    # Name = "teammjk-private-app-rt-${each.key + 1}" # NAT Gateway를 사용하지 않는 경우 주석 처리
     Name = "teammjk-private-app-rt"
   }
 }
@@ -88,7 +85,7 @@ resource "aws_route_table_association" "private_app" {
   for_each = aws_subnet.private_app
 
   subnet_id      = each.value.id
-  route_table_id = aws_route_table.private_app[each.key].id
+  route_table_id = aws_route_table.private_app.id
 }
 
 
@@ -124,30 +121,26 @@ resource "aws_route_table_association" "private_db" {
 
 
 # ====================================================================================
-# Nat Gateway
+# Nat Gateway (단일 AZ))
 # ====================================================================================
 
-# # Elastic IP for NAT Gateway
-# resource "aws_eip" "nat" {
-#   for_each = aws_subnet.public_agent
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  tags = {
+    Name = "teammjk-nat-eip"
+  }
+}
 
-#   domain = "vpc"
-#   tags = {
-#     Name = "teammjk-nat-eip-${each.key + 1}"
-#   }
-# }
+# NAT Gateway
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = values(aws_subnet.public_agent)[0].id
 
-# # NAT Gateway
-# resource "aws_nat_gateway" "main" {
-#   for_each = aws_subnet.public_agent
-
-#   allocation_id = aws_eip.nat[each.key].id
-#   subnet_id     = each.value.id
-
-#   tags = {
-#     Name = "teammjk-nat-gateway-${each.key + 1}"
-#   }
-# }
+  tags = {
+    Name = "teammjk-nat-gateway"
+  }
+}
 
 
 # ====================================================================================
@@ -173,13 +166,9 @@ resource "aws_security_group" "vpc_endpoint" {
 }
 
 resource "aws_vpc_endpoint" "s3" {
-  vpc_id       = aws_vpc.main.id
-  service_name = "com.amazonaws.${var.aws_region}.s3"
+  vpc_id          = aws_vpc.main.id
+  service_name    = "com.amazonaws.${var.aws_region}.s3"
   route_table_ids = [aws_route_table.private_app.id, aws_route_table.private_db.id]
-
-  tags = {
-    Name = "teammjk-s3-vpc-endpoint"
-  }
 }
 
 resource "aws_vpc_endpoint" "ecr_api" {
